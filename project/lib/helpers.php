e402990056ce95fa6186189c4c6dee6b9b7ad952
Sarabveer Singh <me@sarabveer.me>
2020-11-10 23:18:03 -0500

    Move test files to test folder

--- project/lib/helpers.php
+++ project/lib/helpers.php
@@ -76,4 +76,11 @@ function getMessages()
 
 //end flash
 
+function getURL($path) {
+  if(substr($path, 0, 1) == '/') {
+    return $path;
+  }
+  return $_SERVER['CONTEXT_PREFIX'] . "/IT202/project/$path";
+}
+
 ?>
