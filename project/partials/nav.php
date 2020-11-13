ffb062d8cc6f7c13245ac1e321a659dfc6122a76
Sarabveer Singh <me@sarabveer.me>
2020-11-12 23:54:19 -0500

    Add test files to nav.php

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,13 +1,20 @@
-<?php //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
+<?php
+//we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once __DIR__ . "/../lib/helpers.php";
 ?>
 <link rel="stylesheet" href="<?php echo getURL('static/css/styles.css'); ?>">
 <nav>
 <ul class="nav">
-    <li><a href="home.php">Home</a></li>
+    <li><a href="<?php echo getURL('home.php'); ?>">Home</a></li>
     <?php if (!is_logged_in()): ?>
         <li><a href="<?php echo getURL('login.php'); ?>">Login</a></li>
         <li><a href="<?php echo getURL('register.php'); ?>">Register</a></li>
+    <?php endif; ?>
+    <?php if (has_role("Admin")): ?>
+        <li><a href="<?php echo getURL('test/test_create_accounts.php'); ?>">Create Account</a></li>
+        <li><a href="<?php echo getURL('test/test_list_accounts.php'); ?>">View Account</a></li>
+        <li><a href="<?php echo getURL('test/test_create_transactions.php'); ?>">Create Transaction</a></li>
+        <li><a href="<?php echo getURL('test/test_list_transactions.php'); ?>">View Transactions</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="<?php echo getURL('profile.php'); ?>">Profile</a></li>
