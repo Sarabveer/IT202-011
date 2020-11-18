0ee825ed418a36cf5b37ad2a5f33f2a9cf9e7025
Sarabveer Singh <me@sarabveer.me>
2020-11-17 23:56:13 -0500

    Update navbar

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -15,36 +15,39 @@ require_once __DIR__ . "/../lib/helpers.php";
 	<script defer src="https://cdn.jsdelivr.net/npm/bootstrap.native@3/dist/bootstrap-native.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="<?php echo getURL('/'); ?>">Sarabveer's Bank</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="<?php echo getURL('/'); ?>">Sarabveer's Bank</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link" href="<?php echo getURL('home.php'); ?>">Home</span></a></li>
-            <?php if (!is_logged_in()): ?>
-                <li class="nav-item"><a class="nav-link" href="<?php echo getURL('login.php'); ?>">Login</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?php echo getURL('register.php'); ?>">Register</a></li>
-            <?php endif; ?>
-            <?php if (is_logged_in()): ?>
-                <li class="nav-item"><a class="nav-link" href="<?php echo getURL('profile.php'); ?>">Profile</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?php echo getURL('logout.php'); ?>">Logout</a></li>
-            <?php endif; ?>
-            <?php if (has_role("Admin")): ?>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="javascript:void(0);" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Admin
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href=<?php echo getURL('test/test_create_accounts.php'); ?>">Create Account</a>
-                        <a class="dropdown-item" href="<?php echo getURL('test/test_list_accounts.php'); ?>">View Account</a>
-                        <a class="dropdown-item" href="<?php echo getURL('test/test_create_transactions.php'); ?>">Create Transaction</a>
-                        <a class="dropdown-item" href="<?php echo getURL('test/test_list_transactions.php'); ?>">View Transactions</a>
-                    </div>
-                </li>
-            <?php endif; ?>
-            </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a class="nav-link" href="<?php echo getURL('home.php'); ?>">Home</span></a></li>
+                <?php if (!is_logged_in()): ?>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo getURL('login.php'); ?>">Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo getURL('register.php'); ?>">Register</a></li>
+                <?php endif; ?>
+                <?php if (is_logged_in()): ?>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo getURL('profile.php'); ?>">Profile</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo getURL('logout.php'); ?>">Logout</a></li>
+                <?php endif; ?>
+                <?php if (has_role("Admin")): ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="javascript:void(0);" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Admin
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href=<?php echo getURL('test/test_create_accounts.php'); ?>">Create Account</a>
+                            <a class="dropdown-item" href="<?php echo getURL('test/test_list_accounts.php'); ?>">View Account</a>
+                            <a class="dropdown-item" href="<?php echo getURL('test/test_create_transactions.php'); ?>">Create Transaction</a>
+                            <a class="dropdown-item" href="<?php echo getURL('test/test_list_transactions.php'); ?>">View Transactions</a>
+                        </div>
+                    </li>
+                <?php endif; ?>
+                </ul>
+            </div>
         </div>
     </nav>
+    <div class="container">
