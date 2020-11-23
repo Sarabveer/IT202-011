3b459863d71ce62374968ebb9212089f09a2d174
Sarabveer Singh <me@sarabveer.me>
2020-11-22 22:14:42 -0500

    Add accounts list page

--- project/home.php
+++ project/home.php
@@ -3,12 +3,7 @@ require_once __DIR__ . "/partials/nav.php";
 if (!is_logged_in()) {
   //this will redirect to login and kill the rest of this script (prevent it from executing)
   flash("You don't have permission to access this page");
-  die(header("Location: index.php"));
-}
-//we use this to safely get the email to display
-$email = "";
-if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
-  $email = $_SESSION["user"]["email"];
+  die(header("Location: login.php"));
 }
 ?>
     <div class="jumbotron mt-4 text-center">
@@ -18,7 +13,7 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
       <p>
         <a class="btn btn-primary" href="#" role="button"><i class="fas fa-hand-holding-usd fa-fw"></i> Deposit</a>
         <a class="btn btn-primary" href="#" role="button"><i class="fas fa-money-bill-wave fa-fw"></i> Withdraw</a>
-        <a class="btn btn-primary" href="#" role="button"><i class="fas fa-wallet fa-fw"></i> View Accounts</a>
+        <a class="btn btn-primary" href="accounts.php" role="button"><i class="fas fa-wallet fa-fw"></i> View Accounts</a>
         <a class="btn btn-primary" href="#" role="button"><i class="fas fa-file-invoice-dollar fa-fw"></i> Open Account</a>
       </p>
     </div>
