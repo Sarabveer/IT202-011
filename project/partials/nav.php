ab5e2a54715fe5af116f0e6a6669ac49c9e702d9
Sarabveer Singh <me@sarabveer.me>
2020-12-08 18:42:30 -0500

    Update links for transactions

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -32,9 +32,22 @@ require_once __DIR__ . "/../lib/helpers.php";
                 <?php endif; ?>
                 <?php if (is_logged_in()): ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo getURL('home.php'); ?>">Home</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo getURL('accounts.php'); ?>">Accounts</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="javascript:void(0);" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Transaction
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="<?php echo getURL('transaction.php?type=deposit'); ?>">Deposit</a>
+                            <a class="dropdown-item" href="<?php echo getURL('transaction.php?type=withdraw'); ?>">Withdraw</a>
+                        </div>
+                    </li>
+                </ul>
+                <?php endif; ?>
+                <?php if (is_logged_in()): ?>
+                <ul class="navbar-nav ml-auto">
                     <li class="nav-item"><a class="nav-link" href="<?php echo getURL('profile.php'); ?>">Profile</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo getURL('logout.php'); ?>">Logout</a></li>
-                <?php endif; ?>
                 <?php if (has_role("Admin")): ?>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="javascript:void(0);" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -47,6 +60,7 @@ require_once __DIR__ . "/../lib/helpers.php";
                             <a class="dropdown-item" href="<?php echo getURL('test/test_list_transactions.php'); ?>">View Transactions</a>
                         </div>
                     </li>
+                <?php endif; ?>
                 <?php endif; ?>
                 </ul>
             </div>
