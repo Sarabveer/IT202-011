2d3e2f1ee454ce7a17ec36e9b5bcca5428479b22
Sarabveer Singh <me@sarabveer.me>
2020-12-08 18:42:30 -0500

    Update links for transactions

--- project/home.php
+++ project/home.php
@@ -11,8 +11,8 @@ if (!is_logged_in()) {
       <hr class="my-4">
       <p class="lead">What would you like to do today?</p>
       <p>
-        <a class="btn btn-primary" href="#" role="button"><i class="fas fa-hand-holding-usd fa-fw"></i> Deposit</a>
-        <a class="btn btn-primary" href="#" role="button"><i class="fas fa-money-bill-wave fa-fw"></i> Withdraw</a>
+        <a class="btn btn-primary" href="transaction.php?type=deposit" role="button"><i class="fas fa-hand-holding-usd fa-fw"></i> Deposit</a>
+        <a class="btn btn-primary" href="transaction.php?type=withdraw" role="button"><i class="fas fa-money-bill-wave fa-fw"></i> Withdraw</a>
         <a class="btn btn-primary" href="accounts.php" role="button"><i class="fas fa-wallet fa-fw"></i> View Accounts</a>
         <a class="btn btn-primary" href="create_account.php" role="button"><i class="fas fa-file-invoice-dollar fa-fw"></i> Open Account</a>
       </p>
