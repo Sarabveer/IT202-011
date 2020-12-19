8a0962a544e770fd76a872947a298cf977396025
Sarabveer Singh <me@sarabveer.me>
2020-12-19 03:46:23 -0500

    Add index page

--- project/index.php
+++ project/index.php
@@ -1,4 +1,17 @@
-<?php require_once __DIR__ . "/partials/nav.php"; ?>
-This is a placeholder index file for the project.
-Typically this would be a public homepage and I leave it up to the project requirements to fill this in.
-If we didn't have an index file navigating to the project folder would list our files.
+<?php
+ob_start();
+require_once __DIR__ . "/partials/nav.php";
+if (is_logged_in()) {
+  die(header("Location: home.php"));
+}
+ob_end_flush();
+?>
+    <div class="jumbotron mt-4 text-center">
+      <h1 class="display-4">Sarabveer's Bank</h1>
+      <hr class="my-4">
+      <p>
+        <a class="btn btn-primary" href="login.php" role="button">Login</a>
+        <a class="btn btn-primary" href="register.php" role="button">Register</a>
+      </p>
+    </div>
+<?php require __DIR__ . "/partials/flash.php"; ?>
